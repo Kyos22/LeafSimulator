66069be4c14b4aa1b74d4f2b9ad862e89d4b8a0c
Chikitori7th <leviettinh1001@gmail.com>
2026-01-19 21:53:01 +0700

    Update

--- src/Server/Systems/Leaf/init.lua
+++ src/Server/Systems/Leaf/init.lua
@@ -1,11 +1,23 @@
 --!strict
+
+--// Service
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LeafSystem = require(script.leaf)
 local AreaSystem = require(script.Area)
 
 local leafSystem = LeafSystem.new({})
 local areaSystem = AreaSystem.new({})
+--// Modules
+local Yumi = require(ReplicatedStorage.Shared.Core.Yumi)
 
+--
+export type APIsType = {}
 
+local module = {} :: APIsType & Yumi.System
+
+--// Yumi
+
+--// APIs
 areaSystem:Initialize(leafSystem)
 
-
+return module
